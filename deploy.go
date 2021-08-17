7c100d8446f9890b3b00b3a75fd493c3c1b28c82
Eric.Cheng.BlockChain <matsumi_design@163.com>
2021-08-17 16:43:08 +0800

    Update deploy.go
    root@chengdeMacBook-Pro src # go run contract_deploy.go
    
    合约地址:	0x38C0E5B95b2f3D7565B2506607d357180f6f1794
    交易哈希:	0x2fd2bf00e5da86a4290251c3a9c0174d04f28cb6b5d3bbe5497a4468bc0aa752

--- deploy.go
+++ deploy.go
@@ -1,1 +1,63 @@
+package main
+
+import (
+    "context"
+    "crypto/ecdsa"
+    "fmt"
+    "log"
+    "math/big"
+
+    "github.com/ethereum/go-ethereum/accounts/abi/bind"
+    "github.com/ethereum/go-ethereum/crypto"
+    "github.com/ethereum/go-ethereum/ethclient"
+  
+    "inbox"//这个文件夹是在goroot下面新建一个box,在这个box文件夹下面有一个inbox.sol合约生成的inbox.go文件
+)
+
+func main() {
+    client, err := ethclient.Dial("https://kovan.infura.io/v3/9aa3d95b3...你的节点")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    privateKey, err := crypto.HexToECDSA("d48c01744fcae61c9ffeb272895...你的私钥")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    publicKey := privateKey.Public()
+    publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+    if !ok {
+        log.Fatal("cannot assert type: publicKey is not of type *ecdsa.PublicKey")
+    }
+
+    fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+    nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    gasPrice, err := client.SuggestGasPrice(context.Background())
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    auth := bind.NewKeyedTransactor(privateKey)
+    auth.Nonce = big.NewInt(int64(nonce))
+    auth.Value = big.NewInt(0)     // in wei
+    auth.GasLimit = uint64(300000) // in units
+    auth.GasPrice = gasPrice
+
+    input := "1.0"
+    address, tx, instance, err := inbox.DeployInbox(auth, client, input)
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Print("\n合约地址:\t")
+    fmt.Println(address.Hex())   // 0x147B8eb97fD247D06....
+    fmt.Print("交易哈希:\t")
+    fmt.Println(tx.Hash().Hex()) // 0xdae8ba5444eefdc9....
+    fmt.Println("\n\n")
+    _ = instance
+}
 
